519bfd272417f738023a7c48258a2d993c44e63e
Pál András Csokán <ciocanpa87@gmail.com>
2026-02-10 09:42:30 +0100

    BS5-2.1 feat(frontend): make Menu modules from menu items called Slice (#10)

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextRequest, NextResponse } from 'next/server'
 
+const isOnboardingRoute = createRouteMatcher(['/onboarding']);
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)', '/leaderboard', '/support', '/contact'])
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { isAuthenticated, sessionClaims, redirectToSignIn } = await auth()
+
+  if (isAuthenticated && isOnboardingRoute(req)) {
+    return NextResponse.next()
   }
+
+  if (!isAuthenticated && !isPublicRoute(req)) return redirectToSignIn({ returnBackUrl: req.url })
+
+  if (isAuthenticated && !sessionClaims?.metadata?.onboardingComplete) {
+    const onboardingUrl = new URL('/onboarding', req.url)
+    return NextResponse.redirect(onboardingUrl)
+  }
+
+  if (isAuthenticated && !isPublicRoute(req)) return NextResponse.next()
 });
 
 export const config = {
