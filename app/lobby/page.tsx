fa4c4f1f3cf5f43a767cf7a0fe83149d5c6d1125
Paul Andrei Ciocan <ciocanpa87@gmail.com>
2026-03-13 14:28:55 +0100

    BS5-5 feat(db): rename topic branch from convex-clerk to clerk-db

--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -7,11 +7,17 @@ export default function Page() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.lobby}>
-				<div className={styles.messageList}>Display message here</div>
-				<div className={styles.messenger}>Send message here</div>
+				<div className={styles.lobby_message_list}>Display message here</div>
+				<div className={styles.lobby_messenger}>Send message here</div>
 			</div>
-			<div className={styles.players}>Online players list</div>
-			<div className={styles.panel}>Command panel</div>
+			<div className={styles.players}>
+				<div className={styles.players_title}>Online Players</div>
+				<div className={styles.players_list}>List</div>
+			</div>
+			<div className={styles.panel}>
+				<div className={styles.panel_title}>Command Panel</div>
+				<div className={styles.panel_panel}>panel</div>
+			</div>
 		</div>
 	);
 }
